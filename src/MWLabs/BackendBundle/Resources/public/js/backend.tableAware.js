38a328a8ab2adb38eeb0d9122fca2a2810fc5d75
Diego Caponera <diego.caponera@gmail.com>
2012-09-21 03:10:58 +0200

    Datatables formatters.

--- src/MWLabs/BackendBundle/Resources/public/js/backend.tableAware.js
+++ src/MWLabs/BackendBundle/Resources/public/js/backend.tableAware.js
@@ -3,13 +3,13 @@
 	$.extend( $.fn.dataTableExt.oStdClasses, {
 	    "sWrapper": "dataTables_wrapper form-inline"
 	} );	
-	
+
 	TableAware = Controller.extend({
-	
+
 		startup : function(element, options){
 
 			var self = this;
-			
+
 			this._super(element);	
 
 			this._table = $('#table', element);
@@ -17,35 +17,59 @@
 				.modal({ 'keyboard' : false, 'show' : false})
 				.on('show', function(){ $(this).removeAttr('style') })
 				.on('shown', function(){ $('input', this).first().focus() });
-				
+
 			this._entity = this._table.attr('data-entity');	
-			
+
 			this._messages = {
-				
+
 				500 : "We had a problem, please contact webmaster."
-				
+
 			};
-	
+
+			/*===========================================
+			 * Templates
+			 *===========================================*/
 			this._templates = {};
-			
+
 			var tmpl = {
 				newForm : 'form-new',
 				removeForm : 'form-remove',
 				rowActions : 'row-actions'
 			};
-			
+
 			var $t;
-			
+
 			for( key in tmpl)
 			{
 
 				if( ($t = $('#template-' + this._entity + '-' + tmpl[key])).length == 0 )
 					continue;
-					
+
 				this._templates[key] = _.template($t.html());
-				
-			}
-			
+
+			}			
+
+			/*===========================================
+			 * Data Formatters
+			 *===========================================*/			
+			this._formatters = {
+
+				"id" : function(value){
+
+					return self._templates.rowActions({ id : value });
+
+				},
+
+				"boolean" : function(value){
+					return value ? '<i class="icon-ok"></i>' : '-';
+				},
+
+				"date" : function(value){
+					return value;
+				}
+
+			};			
+
 			this._tableOptions = {
 			    'fnInitComplete': function(oSettings, json) {
 					$('.loader', element).hide();
@@ -62,27 +86,25 @@
 				'sAjaxSource' : $(this._table).attr('data-source'),
 				'sAjaxDataProp' : '',
 			    'aoColumnDefs': _.reduce($('thead th[data-m-data]', this.table), function(memo, element){ 
-					
+
 					var props = $(element).data();
 					props.aTargets = [memo.length];
-					
-					if( props.mData == 'id'){
-						$.extend( props, { mRender : function ( data, type, full ) { return self._templates.rowActions({ id : full.id }) } } );
+
+					if( props.type in self._formatters){
+
+						$.extend( props, { mRender : function ( data, type, full ) { return self._formatters[props.type](full[props.mData]) } } );
+
 					}
-					
-					if( props.type == 'boolean'){
-						$.extend( props, { mRender : function ( data, type, full ) { return full[props.mData] ? '<i class="icon-ok"></i>' : '-' } } );						
-					}
-					
+
 					memo.push(props);
 					return memo;
 				}, [])
 		    };
-			
+
 			$.extend(true, this, options);
 
 			this._table.dataTable( this._tableOptions );
-		
+
 			/*===========================================
 			 * Private functions
 			 *===========================================*/
@@ -93,25 +115,25 @@
 				for(key in errors){ $('[name$="[' + key + ']"]', form).after($('<div></div>').html( errors[key] ).addClass('error')) }
 
 			}
-			
+
 			this._getRoute = function($form, type){
-				
+
 				var params = {}, routeParams = $form.data('route-params'), route = type.toLowerCase() + '_';
-				
+
 				for(var i = 0; i < routeParams.length; i++){
-					
+
 					params[ routeParams[i] ] = $("input[name='" + this._entity + "[" + routeParams[i] + "]']", $form).val();
-					
+
 					route += ( i != routeParams.length -1 ) ? routeParams[i] + 's_' : this._entity + 's';
-					
+
 				}
-				
+
 				return Routing.generate( route, params );
-				
+
 			};
-			
+
 			this._restCall = function(form, type, callback){
-			
+
 				var self = this;
 				var $form = $(form);
 				var $button = $('.btn-primary', $form);
@@ -121,19 +143,13 @@
 				else
 					$button.button('loading');
 
-				//var url = Routing.generate( type.toLowerCase() + '_' + this._entity + 's', { id : $("input[name='" + this._entity + "[id]']", $form).val() } );
-				
-				//console.log(url);
-
-				//var url = $form.attr('action') + ( (type == 'PUT' || type == 'DELETE') ? '/' + $("input[name='" + this._entity + "[id]']", $form).val() : '' );
-
 				$.ajax({
 					url : this._getRoute($form, type),
 					type : type,
 					data : $form.serialize(),
 					dataType : 'json',
 					statusCode : {
-						
+
 						200 : function(data, textStatus, jqXHR){
 
 							$('.error', form).html('');
@@ -151,7 +167,7 @@
 							self._table.length > 0 && self._table.dataTable().fnReloadAjax();								
 
 						},
-						
+
 						204 : function(data, textStatus, jqXHR){
 
 							$('.error', form).html('');
@@ -170,7 +186,7 @@
 								$form.find('.alert-danger .message').html(data.message).parent().show();
 
 						},
-						
+
 						403 : function(jqXHR, textStatus, errorThrown){
 
 							var data = $.parseJSON(jqXHR.responseText);
@@ -187,84 +203,84 @@
 						}
 
 					},
-					
+
 					complete : function(jqXHR, textStatus)
 					{
-						
+
 						if(callback)
 							callback.call(self);
 
 					}
 
 				});				
-				
+
 			};
-		
+
 		},
-		
+
 		showNewForm : function(element){
-			
+
 			$(this._modal)
 				.html(this._templates.newForm({ action : 'post'}))
 				.modal('show');
-			
+
 		},
-		
+
 		showEditForm : function(element){
-		
+
 			var entry = this._table.dataTable().fnGetData( $(element).closest('tr')[0] );
 			var $modal = $(this._modal).html(this._templates.newForm({ action : 'put', entry : entry }));
 			var $form = $modal.find('form');
-			
+
 			for(key in entry){
 
 				$div = $form.find('div#' + this._entity + '_' + key);
 				if($div.length > 0){
-					
+
 					for(var i = 0; i < entry[key].length; i++){
-						
+
 						$('input[value="' + entry[key][i] +'"]', $div).prop('checked', true);
-						
+
 					}
-					
+
 				}else{
-				
+
 					$form.find('[name$="[' + key + ']"]').val( typeof(entry[key]) == 'boolean' ? entry[key]+0 : entry[key]);				
-					
+
 				}
-				
+
 			}
-			
+
 			$modal.modal('show');
-			
+
 		},
-		
+
 		showRemoveForm : function(element){
-		
+
 			$(this._modal)
 				.html(this._templates.removeForm( { entry : this._table.dataTable().fnGetData( $(element).closest('tr')[0] ) } ))
 				.modal('show');
-			
+
 		},		
-		
+
 		post : function(form, callback){
-			
+
 			this._restCall(form, 'POST', callback);
 
 		},
-		
+
 		put : function(form, callback){
 
 			this._restCall(form, 'PUT', callback);
-			
+
 		},
-		
+
 		remove : function(form, callback){
-			
+
 			this._restCall(form, 'DELETE', callback);
-			
+
 		}
-		
+
 	});
-	
+
 })();
